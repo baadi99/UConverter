ddeaed499326fc2e2cb03b974678acf56ef4fdb7
Mohcine BAADI <48631610+baadi99@users.noreply.github.com>
2021-11-20 18:49:21 +0100

    Added views and navigation

---
+++ app/src/main/java/com/project/uconverter/MainActivity.java
@@ -0,0 +1,46 @@
+package com.project.uconverter;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Setting up the toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(""); //To remove the title that is set by default to the app name
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.appbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Navigation.findNavController(MainActivity.this, R.id.nav_host_fragment).navigate(R.id.action_homePageFragment_to_settingsFragment);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
