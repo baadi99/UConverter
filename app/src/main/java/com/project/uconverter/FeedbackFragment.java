1e778e06679618b00e158ab2c2edf30ef57429b2
Mohcine BAADI <48631610+baadi99@users.noreply.github.com>
2021-12-08 23:26:40 +0100

    fixed conflict

--- app/src/main/java/com/project/uconverter/FeedbackFragment.java
+++ app/src/main/java/com/project/uconverter/FeedbackFragment.java
@@ -1,49 +1,28 @@
 package com.project.uconverter;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.KeyEvent;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
 
-import androidx.annotation.ArrayRes;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.project.uconverter.units.Area;
-import com.project.uconverter.units.Converter;
-import com.project.uconverter.units.Length;
-import com.project.uconverter.units.Mass;
-import com.project.uconverter.units.Temperature;
-import com.project.uconverter.units.Time;
-import com.project.uconverter.units.Utils;
-import com.project.uconverter.units.Volume;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class ConverterFragment extends Fragment {
+import java.util.regex.Pattern;
 
-    // UI items
-    private TextView resultView;
-    private EditText valueView;
-    private Spinner fromUnitDropDown;
-    private Spinner toUnitDropDown;
 
-    // Spinner Adapter
-    private ArrayAdapter<CharSequence> dropDownAdapter;
-
-    // A variable to store the converter instance of one of the implementations
-    // of the Converter interface and use polymorphism to call the appropriate
-    // implementation
-    private Converter converter;
+public class FeedbackFragment extends Fragment {
 
     // Required empty public constructor
-    public ConverterFragment() {
-    }
+    public FeedbackFragment() { }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -52,153 +31,62 @@ public class ConverterFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        // label is passed from the home page fragments and indicates the label of
-        // the conversion that was chosen (i.e. Length, Area, ...) and is used to
-        // initialize spinners with the appropriate data and to use the appropriate
-        // conversion algorithm
-        String label = ConverterFragmentArgs.fromBundle(getArguments()).getLabel();
-
-        // a variable to store the array resource (Defined in res > values > arrays.xml)
-        // that will be used to initialize spinner
-        @ArrayRes
-        int units_resource;
-
-        switch (label) {
-            case "length":
-                units_resource = R.array.length_units;
-                converter = Length.getInstance();
-                break;
-            case "mass":
-                units_resource = R.array.mass_units;
-                converter = Mass.getInstance();
-                break;
-            case "area":
-                units_resource = R.array.area_units;
-                converter = Area.getInstance();
-                break;
-            case "volume":
-                units_resource = R.array.volume_units;
-                converter = Volume.getInstance();
-                break;
-            case "time":
-                units_resource = R.array.time_units;
-                converter = Time.getInstance();
-                break;
-            case "temperature":
-                units_resource = R.array.temperature_units;
-                converter = Temperature.getInstance();
-                break;
-            default:
-                units_resource = R.array.length_units;
-        }
-
-        dropDownAdapter = ArrayAdapter.createFromResource(container.getContext(), units_resource,
-                android.R.layout.simple_spinner_dropdown_item);
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_converter, container, false);
+        return inflater.inflate(R.layout.fragment_feedback, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        // Set up from unit spinner
-        fromUnitDropDown = view.findViewById(R.id.from_unit_view);
-        dropDownAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        fromUnitDropDown.setAdapter(dropDownAdapter);
-        int nmUnits = fromUnitDropDown.getCount();
-        fromUnitDropDown.setSelection(nmUnits / 2); // Set default from unit to be meter
+        EditText emailView = view.findViewById(R.id.email);
+        EditText messageView = view.findViewById(R.id.message);
+        Button sendBtn = view.findViewById(R.id.button_send);
 
-        // Set up to unit spinner
-        toUnitDropDown = view.findViewById(R.id.to_unit_view);
-        toUnitDropDown.setAdapter(dropDownAdapter);
+        sendBtn.setOnClickListener((View v) -> {
+            // Get form data
+            String email = emailView.getText().toString().trim();
+            String message = messageView.getText().toString().trim();
 
-        // initialize result
-        resultView = view.findViewById(R.id.result);
-        valueView = view.findViewById(R.id.value);
+            //if email is valid clear error
+            emailView.setError(null);
+            if (email.isEmpty()) {
+                emailView.setError("Email is Required!");
+            } else if (!isEmailValid(email)) {
+                emailView.setError("Invalid Email!");
+            } else {
+                //if email is valid clear error
+                emailView.setError(null);
 
-        // Set an event on the value field
-        valueView.setOnKeyListener((View v, int keyCode, KeyEvent event) -> {
-
-            // Check key pressed is in the interval 0 - 9 or it's a backspace (<- key) or a
-            // dot "."
-            // For decimal values.
-            if ((keyCode >= 7 && keyCode <= 16) || (keyCode == 67) || (keyCode == 158)) {
-                String input = valueView.getText().toString();
-
-                if (input.length() == 0) {
-                    // Reset text view when input is cleared
-                    resultView.setText("0");
+                if (message.isEmpty()) {
+                    messageView.setError("Message can't be empty!");
                 } else {
-                    calculateResult();
+
+                    Intent sendEmailIntent = new Intent(Intent.ACTION_SEND);
+                    sendEmailIntent.setType("message/rfc822")
+                            .setPackage("com.google.android.gm")
+                            .putExtra(Intent.EXTRA_EMAIL, new String[]{"sakhrihamza9@gmail.com", "mohsin.baadi99@gmail.com"})
+                            .putExtra(Intent.EXTRA_SUBJECT, "Uconverter App Feedback")
+                            .putExtra(Intent.EXTRA_TEXT, message);
+
+                    try {
+                        startActivity(Intent.createChooser(sendEmailIntent, "Send mail..."));
+                    } catch (ActivityNotFoundException e) {
+                        Toast.makeText(getActivity(), "Gmail application is not installed on this device", Toast.LENGTH_SHORT).show();
+                    }
                 }
-                return false;
-                // when the back key is pressed remove focus on value field
-                // this unblocks going back action
-            } else if (keyCode == 4) {
-                resultView.setFocusable(false);
-                return false;
-            }
-            return true;
-        });
-
-        // handle the switch button event
-        view.findViewById(R.id.switch_units_btn).setOnClickListener((View v) -> {
-            int fromUnit = fromUnitDropDown.getSelectedItemPosition();
-            fromUnitDropDown.setSelection(toUnitDropDown.getSelectedItemPosition());
-            toUnitDropDown.setSelection(fromUnit);
-            // Update value
-            calculateResult();
-        });
-
-        // Update result when a new from unit is selected
-        fromUnitDropDown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                calculateResult();
             }
 
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-
-        // Update result when a new to unit is selected
-        toUnitDropDown.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                calculateResult();
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
         });
 
     }
 
-    /*
-     * A helper method that is responsible for calling the appropriate conversion
-     * algorithm and formatting the resulting value
-     */
-    private void calculateResult() {
-        String input = valueView.getText().toString();
-        if (input.length() > 0) {
-            // Get the selected items (from unit and to unit)
-            String fromUnit = fromUnitDropDown.getSelectedItem().toString();
-            String toUnit = toUnitDropDown.getSelectedItem().toString();
+    private boolean isEmailValid(String email) {
 
-            double val = Double.parseDouble(input);
+        final String regEx = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$";
+        Pattern pattern = Pattern.compile(regEx);
 
-            try {
-
-                double result = converter.convert(val, fromUnit, toUnit);
-                resultView.setText(Utils.format(result));
-
-            } catch (Exception e) {
-                resultView.setText(e.getMessage());
-            }
-        }
+        return pattern.matcher(email).matches();
     }
 }
